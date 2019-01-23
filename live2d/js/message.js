19e738e3fe21acb53c55c1875890009893d43968
reveris <KP.khdong@outlook.com>
2019-01-23 16:00:14 +0800

    1

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -112,13 +112,13 @@ function showHitokoto(){
 function showMessage(text, timeout){
     if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
     console.log(text);
-    $('.waifu-tips').stop();
-    $('.waifu-tips').html(text).fadeTo(200, 1);
+    $('.message').stop();
+    $('.message').html(text).fadeTo(200, 1);
     if (timeout === null) timeout = 5000;
     hideMessage(timeout);
 }
 function hideMessage(timeout){
-    $('.waifu-tips').stop().css('opacity',1);
+    $('.message').stop().css('opacity',1);
     if (timeout === null) timeout = 5000;
-    $('.waifu-tips').delay(timeout).fadeTo(200, 0);
+    $('.message').delay(timeout).fadeTo(200, 0);
 }
